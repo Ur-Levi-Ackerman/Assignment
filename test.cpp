5f059ecd782f869959b29d8df447d7117dcffaff
Ur-Levi-Ackerman <myothant13465@gmail.com>
2024-06-06 11:51:46 +0630

    testing

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a1,a2,a3;
+    cout<<"Enter values of angle\n";
+    cin>>a1>>a2;
+    a3=180-(a1+a2);
+    cout<<"Value of angle 3 = "<<a3<<endl;
+}
